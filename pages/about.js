2b6f61f370f2e612bfbe0cc365a21ca734ee3554
shriNiwash <shriniwashsharma98413@gmail.com>
2023-06-21 20:23:27 +0545

    theresolved

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function about() {
   return (
     <>
         <Head>
-      <title>\Contact US</title>
+      <title>Contact US</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
 
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -4,14 +4,14 @@ const Navbar = () =>{
     return(<>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">SMART SCHOOL</a>
+    <a className="navbar-brand" href="#">The Stone Phone Center Pokhara</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link href="/" legacyBehavior><a className="nav-link active" aria-current="page" >Home</a></Link>
+          <Link href="/home" legacyBehavior><a className="nav-link active" aria-current="page" >Home</a></Link>
         </li>
         <li className="nav-item">
           <Link href="/about" legacyBehavior><a className="nav-link" >About</a></Link>
