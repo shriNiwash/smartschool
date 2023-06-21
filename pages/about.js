f339fcd6761c7a205925f3855a0ab4ae938461f8
shriNiwash <shriniwashsharma98413@gmail.com>
2023-06-21 20:19:46 +0545

    the page resolved

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
+import Head from 'next/head'
 
 export default function about() {
   return (
     <>
+        <Head>
+      <title>\Contact US</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+
+    </Head>
     <Navbar />
     <div>
         <h1>about Page</h1>
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import Head from 'next/head';
 
 export default function contact() {
   return (
     <>
+    <Head>
+      <title>Contact US</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+
+    </Head>
     <Navbar />
     <div>
         <h1>Contact Page</h1>
